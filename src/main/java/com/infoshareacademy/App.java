cc6cc597d3271bbbe57019da2fb06036b86282eb
Zbigniew Kupiec <zk-design@outlook.com>
2022-03-26 14:04:33 +0100

    update and fix class App, Employee and Holiday

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,8 +1,12 @@
 package com.infoshareacademy;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,14 +18,31 @@ public class App {
     public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
     public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
     public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    private String startDate;
+    private String endDate;
     String etc = "";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         // write your code here
-        new App().displayMenu();
+        new App().window();
+    }
+    public void window() throws IOException, ParseException {
+        System.out.printf("\n\n%10s*****************************************************\n", etc);
+        System.out.printf("%10s*\t               Holidays v0.1.2022%16s *", etc, etc);
+        System.out.printf("\n%10s*****************************************************\n", etc);
+        System.out.printf("%10s*\t1. Wejdź do aplikacji %28s*\n", etc, etc);
+        System.out.printf("%10s*\t0. Wyjście z aplikacji %27s*\n", etc, etc);
+        System.out.printf("%10s*****************************************************\n", etc);
+        System.out.printf("%10s*\tWybierz opcję 1 lub 0 : ",etc);
+        int option = new Scanner(System.in).nextInt();
+        switch (option) {
+            case 1 -> displayMenu();
+            case 0 -> System.exit(0);
+            default -> System.out.println("Zła opcja, wybierz inną.");
+        }
     }
 
-    public void displayMenu() throws IOException {
+    public void displayMenu() throws IOException, ParseException {
         System.out.printf("\n\n%10s*****************************************************\n", etc);
         System.out.printf("%10s*\t               Witaj w Holidays App%15s*", etc, etc);
         System.out.printf("\n%10s*****************************************************\n", etc);
@@ -29,18 +50,19 @@ public class App {
         System.out.printf("%10s*****************     LOGOWANIE    ******************\n", etc);
         System.out.printf("%10s*\t1. Zaloguj się jako Pracownik%21s*\n", etc, etc);
         System.out.printf("%10s*\t2. Zalogij się jako Pracodawca / Admin%12s*\n", etc, etc);
-        System.out.printf("%10s*\t3. Wyjście %39s*\n", etc, etc);
+        System.out.printf("%10s*\t0. Powrót %40s*\n", etc, etc);
         System.out.printf("%10s*****************************************************\n", etc);
-        System.out.printf("%10s*\tWybierz opcję 1 lub 2, 3 to Wyjście : ",etc);
+        System.out.printf("%10s*\tWybierz opcję 0 - 2 : ",etc);
         int option = new Scanner(System.in).nextInt();
         switch (option) {
             case 1 -> loginEmployee();
             case 2 -> loginOwner();
-            case 3 -> System.exit(0);
+            case 0 -> window();
             default -> System.out.println("Zła opcja, wybierz inną.");
         }
     }
-    private void loginEmployee() throws IOException {
+
+    private void loginEmployee() throws IOException, ParseException {
         int employee;
         ArrayList<String> list = new ArrayList<>();
         do {
@@ -53,9 +75,9 @@ public class App {
             System.out.printf("%10s*\t2. Złóź wniosek urlopowy %25s*\n", etc, etc);
             System.out.printf("%10s*\t3. Znajdz Pracownika %29s*\n", etc, etc);
             System.out.printf("%10s*\t4. Pokaż wszystkich pracowników%19s*\n", etc, etc);
-            System.out.printf("%10s*\t5. Wyloguj się %35s*\n", etc, etc);
+            System.out.printf("%10s*\t0. Wyloguj się %35s*\n", etc, etc);
             System.out.printf("%10s*****************************************************\n", etc);
-            System.out.printf("%10s*\tWybierz opcję 1 - 5 : ", etc);
+            System.out.printf("%10s*\tWybierz opcję 0 - 4 : ", etc);
             employee = new Scanner(System.in).nextInt();
             switch(employee) {
                 case 1 -> calendarPrint();
@@ -86,12 +108,13 @@ public class App {
                         System.out.println(s);
                     }
                 }
-                case 5 -> displayMenu();
+                case 0 -> displayMenu();
             }
         } while (employee != 5);
 
     }
-    public void holidays() {
+    
+    public void holidays() throws ParseException, IOException {
         System.out.printf("\n%10s*****************************************************\n", etc);
         System.out.printf("%10s*\t                WNIOSEK O URLOP %17s *", etc, etc);
         System.out.printf("\n%10s*****************************************************\n", etc);
@@ -133,41 +156,28 @@ public class App {
             case 9 -> System.out.println("Wybrano : " + Department.DEPARTMENT_9.getName());
             case 10 -> System.out.println("Wybrano : " + Department.DEPARTMENT_10.getName() + "\n");
         }
-        System.out.printf("%10s* ", " ");
-        System.out.println("Podaj pierwszą datę : ");
+        @SuppressWarnings("resource")
+        Scanner in = new Scanner(new InputStreamReader(System.in));
         System.out.printf("%10s* ", "");
-        System.out.print("Dzień : ");
-        int dayOne = scn.nextInt();
+        System.out.print("Podaj początkową datę (DD/MM/YY) : ");
+        startDate = in.next();
         System.out.printf("%10s* ", "");
-        System.out.print("Miesiąc : ");
-        int monthOne = scn.nextInt();
-        System.out.printf("%10s* ", "");
-        System.out.print("Rok  : ");
-        int yearOne = scn.nextInt();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(yearOne,monthOne - 1,dayOne);
-        System.out.println();
-        System.out.printf("%10s* ", "");
-        System.out.println("Podaj końcowa datę : ");
-        System.out.printf("%10s* ", "");
-        System.out.print("Dzień : ");
-        int dayNext = scn.nextInt();
-        System.out.printf("%10s* ", "");
-        System.out.print("Miesiąc : ");
-        int monthNext = scn.nextInt();
-        System.out.printf("%10s* ", "");
-        System.out.print("Rok : ");
-        int yearNext = scn.nextInt();
-
-        Calendar calendarNext = Calendar.getInstance();
-        calendarNext.set(yearNext,monthNext - 1,dayNext);
-
-        long timeInMillis = calendar.getTimeInMillis();
-        long timeInMillisTwo = calendarNext.getTimeInMillis();
-        long day = timeInMillisTwo - timeInMillis;
-        long days = day / (24 * 60 * 60 * 1000);
-        long m_r = (26 - days);
-
+        System.out.print("Podaj końcową datę (DD/MM/YY) : ");
+        endDate = in.next();
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar start = Calendar.getInstance();
+            start.setTime(df.parse(startDate));
+            Calendar end = Calendar.getInstance();
+            end.setTime(df.parse(endDate));
+            int freeDays = 0;
+            int freeHoliday = 0;
+            while(!start.after(end)) {
+                int day = start.get(Calendar.DAY_OF_WEEK);
+                if ((day != Calendar.SATURDAY) && (day != Calendar.SUNDAY))
+                    freeDays++;
+                start.add(Calendar.DATE, 1);
+                freeHoliday = (26 - freeDays);
+            }
         System.out.println();
         System.out.printf("\n%10s*****************************************************\n", etc);
         System.out.printf("%10s*\t                WNIOSEK URLOPOWY%18s*", etc, etc);
@@ -177,12 +187,11 @@ public class App {
         System.out.printf("%10s*\t ", "");
         System.out.println("Imię i Nazwisko : " + CYAN_BOLD + name + " " + surname + ANSI_RESET);
         System.out.printf("%10s*\t ", "");
-        System.out.println("Wybrałeś : " + GREEN_BOLD + days + " dni" + ANSI_RESET);
+        System.out.println("Wybrałeś : " + GREEN_BOLD + freeDays + " dni" + ANSI_RESET);
         System.out.printf("%10s*\t ", "");
-        System.out.println("Zakres : " + GREEN_BOLD + dayOne + "/" + monthOne + "/" + yearOne + " do " + dayNext + "/"
-                + monthNext + "/" + yearNext + ANSI_RESET);
+        System.out.println("Zakres : " + GREEN_BOLD + startDate + " do " + endDate + ANSI_RESET);
         System.out.printf("%10s*\t ", "");
-        System.out.println("Zostało : " + YELLOW_BOLD + m_r + " dni" + ANSI_RESET);
+        System.out.println("Zostało : " + YELLOW_BOLD + freeHoliday + " dni" + ANSI_RESET);
         System.out.printf("%10s*****************************************************\n", etc);
     }
     private static void calendarPrint() throws IOException {
@@ -195,13 +204,12 @@ public class App {
                 System.out.printf("%10s", "");
                 System.out.println(dayOffList.get(i));
             }
-
         } catch(IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    private void loginOwner() throws IOException {
+    private void loginOwner() throws IOException, ParseException {
         int owner;
         String name;
         ArrayList<String> list = new ArrayList<>();
@@ -217,9 +225,9 @@ public class App {
             System.out.printf("%10s*\t4. Znajdź Pracownika %29s*\n", etc, etc);
             System.out.printf("%10s*\t5. Dodaj Pracownika %30s*\n", etc, etc);
             System.out.printf("%10s*\t6. Pokaż wszystkich pracowników%19s*\n", etc, etc);
-            System.out.printf("%10s*\t7. Wyloguj się %35s*\n", etc, etc);
+            System.out.printf("%10s*\t0. Wyloguj się %35s*\n", etc, etc);
             System.out.printf("%10s*****************************************************\n", etc);
-            System.out.printf("%10s*\tWybierz opcję 1 - 7 : ", etc);
+            System.out.printf("%10s*\tWybierz opcję 0 - 6 : ", etc);
             owner = new Scanner(System.in).nextInt();
             switch(owner) {
                 case 1 -> calendarPrint();
@@ -273,7 +281,7 @@ public class App {
                         System.out.println(s);
                     }
                 }
-                case 7 -> displayMenu();
+                case 0 -> displayMenu();
             }
         } while (owner != 7);
     }
--- src/main/java/com/infoshareacademy/Employee.java
+++ src/main/java/com/infoshareacademy/Employee.java
@@ -2,4 +2,16 @@ package com.infoshareacademy;
 
 public class Employee {
     public int idEmployee;
+
+    public Employee(int idEmployee) {
+        this.idEmployee = idEmployee;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setIdEmployee(int idEmployee) {
+        this.idEmployee = idEmployee;
+    }
 }
